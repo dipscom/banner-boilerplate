bc4f9632ccaef2266e378d925cdf5ee514a3e0a6
Dipscom <pedro@pedrotavares.com>
2015-08-15 19:09:22 +0100

    Setting up the 'check' task

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,30 @@
   'use strict';
   var
     gulp = require('gulp'),
-    plugin =  require('gulp-load-plugins')({ lazy: true });
+    plugin =  require('gulp-load-plugins')({ lazy: true }),
+    path = require('path'),
+    fileSystem = require('fs'),
+    foldersPath = 'src/';
+
+  function getFolders(dir) {
+    return fileSystem
+      .readdirSync(dir)
+      .filter( function(file) {
+        return fileSystem.statSync(path.join(dir, file)).isDirectory();
+      });
+  }
 
   gulp.task('check', function() {
+    // Get the path for the foldersPath
+    var folders = getFolders(foldersPath);
+    //
+    var tasks = folders.map(
+      function(folder) {
+        return gulp
+          .src([path.join(foldersPath, 'shared/*.js'), path.join(foldersPath, folder, '*.js')])
+          .pipe(plugin.concat('Main.js'))
+          .pipe(dest(path.join('build/', folder)));
+    });
     // Check and verify all the JS file against errors
   });
 
