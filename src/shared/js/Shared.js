4c92a6b17f01fe5173e47f1fabf270a1ff1306a2
Pedro Tavares <pedro@pedrotavares.com>
2016-01-26 10:04:39 +0000

    Added a DOMContentLoaded listener

--- src/shared/js/Shared.js
+++ src/shared/js/Shared.js
@@ -1,7 +1,19 @@
 /* Shared.js */
-window.onload = function() {
+Ad = function() {
   'use strict';
   // add the buttons
   var btns = new Button(["clicktag"]);
 };
+
+// wait until DOM is ready
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  // wait until window, stylesheets, images, links, and other media assets are loaded
+  window.onload = function() {
+
+    // All ready, start!
+    Ad();
+
+  };
+});
 /* End of Shared.js */
